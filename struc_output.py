4a25930c9630f18bb6c857c534ce0e9e9c4454e4
abhinay7047 <dubeyabhinay999@gmail.com>
2025-03-20 09:42:30 +0530

    using pydantic

--- struc_output.py
+++ struc_output.py
@@ -6,9 +6,9 @@ class Student(BaseModel):
     age: Optional[int] = None 
     roll: Optional[int] = None
     email: Optional[EmailStr] = None
-    Cgpa : Optional[float] = Field(None,ge=0,le=10)
+    Cgpa : Optional[float] = Field(None,ge=0,le=10, description="CGPA of student")
 
 new_student={"name":'John',"age":20,"roll":101,"email":"abs@gmail.com", 'cgpa':9.5}
 student = Student(**new_student)
-print(student) 
-print(type(student))
+student_dict=dict(student)
+print(student_dict['age'])
